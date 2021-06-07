a61160bb8ee180350cfa6639cfe74cbc9b8857d4
Simon Skinner <vultuk28@gmail.com>
2021-06-07 14:41:45 +0100

    Types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export type { default as Brand } from './brand';
 export type { default as Merchant } from './merchant';
 export type { default as Location } from './location';
+export type { default as Card } from './card';
