fbbaf2f0fdaea1545cec5036c14050731424cb7b
Simon Skinner <vultuk28@gmail.com>
2021-06-08 10:33:25 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export type { default as Brand } from './brand';
 export type { default as Merchant } from './merchant';
 export type { default as Location } from './location';
 export type { default as Card } from './card';
+export type { default as Transaction } from './transaction';
---
+++ src/transaction.ts
@@ -0,0 +1,28 @@
+import Brand from './brand';
+import Card from './card';
+import Merchant from './merchant';
+
+type Transaction = {
+  id?: string;
+  connectionId?: string;
+  brandId?: string;
+  brand?: Brand;
+  created: string;
+  updated: string;
+  offer?: any;
+  approvalCode: string;
+  auth: boolean;
+  authCode: string;
+  cleared: boolean;
+  amount: number;
+  currency: string;
+  wallet?: any;
+  card: Card;
+  merchantId?: string;
+  merchant?: Merchant;
+  locationId?: string;
+  location?: Location;
+  identifiers: any;
+};
+
+export default Transaction;
