a72a90abbd7140face81b6512d881057a8e14f3a
Simon Skinner <vultuk28@gmail.com>
2021-06-11 08:59:47 +0100

    Added long and lat to locations

--- src/location.ts
+++ src/location.ts
@@ -2,10 +2,13 @@ import Merchant from './merchant';
 
 type Location = {
   id?: string;
+  brandId?: string;
   connectionId?: string;
   merchantId: string;
   merchant?: Merchant;
   address1: string;
+  longitude?: number;
+  latitude?: number;
   town: string;
   postalCode: string;
   country: string;
